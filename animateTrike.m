ef810c57bc407935d41447385670327a21fd75ff
unknown <jspear@9F1NT32.olin.edu>
2016-11-28 11:04:37 -0500

    created new views, tuned parameters

--- animateTrike.m
+++ animateTrike.m
@@ -1,35 +1,70 @@
 %animateTrike.m
-clear all;
-initial_angle = pi/4; % radians
-xDotDot = 20; % m/s^2
-D = 0.5; % meters
-[time, stocks] = simulateTrike(D, initial_angle, xDotDot);
+function animateTrike()
+    clear all;
+    initial_angle = (pi/4); % radians
+    D = 0.5; % meters
+    [time, stocks] = simulateTrike(D, initial_angle, @inputFunction);
 
-thetas = stocks(:,1);
-x = time.^2 / 2 * xDotDot;
+    new_timesteps = linspace(time(1), time(end), 100);
+    % Linearly interpolate points so that our theta values are evenly spaced in
+    % time
+    thetas = interp1(time, stocks(:,1), new_timesteps);
+    thetaDots = interp1(time, stocks(:,2), new_timesteps);
 
-x_pivot = x;
-y_pivot = zeros(size(x));
-x_com = x + D.*cos(thetas);
-y_com = D.*sin(thetas);
-figure;
-min_x = min([x_pivot; x_com]);
-max_x = max([x_pivot; x_com]);
-min_y = min([y_pivot; y_com]);
-max_y = max([y_pivot; y_com]);
+    xDotDot = inputFunction(new_timesteps, thetas, thetaDots);
+    xDot = cumtrapz(new_timesteps, xDotDot);
+    x = cumtrapz(new_timesteps, xDot);
 
-loops = numel(thetas);
-F(loops) = struct('cdata',[],'colormap',[]);
-for j = 1:loops
+    x_pivot = x;
+    y_pivot = zeros(size(x));
+    x_com = x + D.*cos(thetas);
+    y_com = D.*sin(thetas);
+    figure;
+    min_x = min([x_pivot, x_com]);
+    max_x = max([x_pivot, x_com]);
+    min_y = min([y_pivot, y_com]);
+    max_y = max([y_pivot, y_com]);
+    
+    plot(new_timesteps, xDotDot);
 
-    Xs = [x_pivot(j); x_com(j)];
-    Ys = [y_pivot(j); y_com(j)];
-    plot(Xs, Ys, 'o', Xs, Ys);
-    axis equal
-    xlim([min_x, max_x])
-    ylim([min_y, max_y])
-    drawnow
-    F(j) = getframe;
+    loops = numel(thetas);
+    panningView(loops, x_pivot, x_com, y_pivot, y_com, min_y, max_y, D)
+%     stationaryView(loops, x_pivot, x_com, y_pivot, y_com, min_y, max_y, min_x, max_x, D)
 end
 
-% movie(F,1,15)
+function xDotDot = inputFunction(t, theta, thetaDot)
+    a = 350;
+    %b = 100000;
+    b = 100000;
+    xDotDot = t.^0 .* a .* ((pi/2 - 0.1) - theta) - b.*thetaDot;
+end
+
+function panningView(loops, x_pivot, x_com, y_pivot, y_com, min_y, max_y, D)
+    F(loops) = struct('cdata',[],'colormap',[]);
+    for j = 1:loops
+
+        Xs = [x_pivot(j); x_com(j)];
+        Ys = [y_pivot(j); y_com(j)];
+        plot(Xs, Ys, 'o', Xs, Ys);
+        axis equal
+        xlim([x_com(j) - D * 2, x_com(j) + D * 2])
+        ylim([min_y, max_y])
+        drawnow
+        F(j) = getframe;
+    end
+end
+
+function stationaryView(loops, x_pivot, x_com, y_pivot, y_com, min_y, max_y, min_x, max_x, D)
+    F(loops) = struct('cdata',[],'colormap',[]);
+    for j = 1:loops
+
+        Xs = [x_pivot(j); x_com(j)];
+        Ys = [y_pivot(j); y_com(j)];
+        plot(Xs, Ys, 'o', Xs, Ys);
+        axis equal
+        xlim([min_x - D * 2, max_x + D * 2])
+        ylim([min_y, max_y])
+        drawnow
+        F(j) = getframe;
+    end
+end
--- simulateTrike.m
+++ simulateTrike.m
@@ -2,20 +2,21 @@
 function [time, stocks] = simulateTrike(D, initial_angle, xDotDot)
     close all;
     mass = 100; % kg
-    time_limit = 100; % seconds
+    time_limit = 10; % seconds
     g = 9.8; % m/s^2
     I = (1/12) * mass * (2*D)^2;
+    beta = 0.05;
 
     options = odeset('Events', @events);
     
     [time, stocks] = ode45(@netFlow, [0, time_limit], [initial_angle; 0], options);
     
     
-    function flows = netFlow(~, stocks)
+    function flows = netFlow(t, stocks)
         theta = stocks(1);
         thetaDot = stocks(2);
         theta_flow = thetaDot;
-        thetaDot_flow = (xDotDot*sin(theta) - g * cos(theta))/((I/mass*D) + D);
+        thetaDot_flow = (xDotDot(t,theta,thetaDot)*sin(theta) - g * cos(theta) - beta * thetaDot)/((I/mass*D) + D);
         flows = [theta_flow; thetaDot_flow];
     end
 
