2528ea3d8f2cf1729ed67b552f2c502d511653ac
unknown <jspear@9F1NT32.olin.edu>
2016-12-08 10:26:32 -0500

    made it so pivot of trike does not intersect with ground

--- animateTrike.m
+++ animateTrike.m
@@ -25,28 +25,27 @@ function animateTrike()
     min_y = min([y_pivot, y_com]);
     max_y = max([y_pivot, y_com]);
     
-%     plot(new_timesteps, thetas);
+     plot(new_timesteps, xDot);
 
     loops = numel(thetas);
-    panningView(loops, x_pivot, x_com, y_pivot, y_com, min_y, max_y, D, thetas, initial_angle)
+%     panningView(loops, x_pivot, x_com, y_pivot, y_com, min_y, max_y, D, thetas, initial_angle)
 %     stationaryView(loops, x_pivot, x_com, y_pivot, y_com, min_y, max_y, min_x, max_x, D)
 
-%      drawRider(D, x_pivot(1), thetas(1))
 end
 
 function xDotDot = inputFunction(t, theta, thetaDot)
-    a = 13;
+    a = 17;
     b = 0;
     xDotDot = t.^0 .* a .* ((pi/2) - theta) - b.*thetaDot;
 end
 
 function panningView(loops, x_pivot, x_com, y_pivot, y_com, min_y, max_y, D, thetas, initial_angle)
     F(loops) = struct('cdata',[],'colormap',[]);
-    for j = 1:1
+    for j = 1:loops
         clf
         Xs = [x_pivot(j); x_com(j)];
         Ys = [y_pivot(j); y_com(j)];
-        drawRider(D, thetas(j) - initial_angle, x_pivot(j));
+        drawRider(D, thetas(j) - initial_angle, x_pivot(j), 0);
         xlim([x_pivot(j) - D * 1, x_pivot(j) + D * 3])
         ylim([0, 2*D])
         drawnow
@@ -70,7 +69,7 @@ function stationaryView(loops, x_pivot, x_com, y_pivot, y_com, min_y, max_y, min
     end
 end
 
-function drawRider(D, angle, x)
+function drawRider(D, angle, x, y)
     hold on;
     unitHeight = D * sin(pi/4);
     unitLength = D * cos(pi/4);
@@ -78,17 +77,21 @@ function drawRider(D, angle, x)
     % Draw line to COM
     COM_x = [0, 1];
     COM_y = [0, 1];
-%     plot(COM_x * unitLength + x, COM_y * unitHeight)
-    
-    [new_COM_x, new_COM_y] = rotate(COM_x, COM_y, angle);
-    plot(new_COM_x * unitLength + x, new_COM_y * unitHeight)
-    
-    
+
     % Points on Bike
     back_hub_x = 0;
     back_hub_y = 1/3;
     front_hub_x = 2;
     front_hub_y = 1/3;
+    [back_wheel_xs, back_wheel_ys] = getCircle(back_hub_x, back_hub_y, (1/3));
+    [front_wheel_xs, front_wheel_ys] = getCircle(front_hub_x, front_hub_y, (1/3));
+    
+    [new_back_wheel_xs, new_back_wheel_ys] = rotate(back_wheel_xs, back_wheel_ys, angle);
+    
+    %Find the point that is touching the ground;
+    [pivot_y, pivot_index] = min(new_back_wheel_ys);
+    pivot_x = new_back_wheel_xs(pivot_index);
+    
     crank_x = 0.9;
     crank_y = (1/3)*1.5;
     seat_stay_x = 0.8;
@@ -124,68 +127,59 @@ function drawRider(D, angle, x)
     shoulder_y = (shoulder_x - hip_x)*back_angle + hip_y;
     
     
-    
-    
-    
-    % Create wheel points
-    [back_wheel_xs, back_wheel_ys] = getCircle(back_hub_x, back_hub_y * unitHeight, (1/3) * unitHeight);
-    [front_wheel_xs, front_wheel_ys] = getCircle(front_hub_x*unitLength, front_hub_y * unitHeight, (1/3) * unitHeight);
-    
-%     plot(back_wheel_xs  + x, back_wheel_ys, 'k');
-%     plot(front_wheel_xs + x, front_wheel_ys, 'k');
-    
-    %Plot the wheels rotated
-    [new_back_xs , new_back_ys] = rotate(back_wheel_xs, back_wheel_ys, angle);
-    [new_front_xs, new_front_ys] = rotate(front_wheel_xs, front_wheel_ys, angle);
-    plot(new_back_xs  + x, new_back_ys, 'k');
-    plot(new_front_xs + x, new_front_ys, 'k');
-    
+    % Draw line to COM
+%     plotRotated(COM_x, COM_y, angle, x, y);
+
+    % Plot Wheels
+    plotRotated(back_wheel_xs, back_wheel_ys, angle, x, y);
+    plotRotated(front_wheel_xs, front_wheel_ys, angle, x, y);
+
     % Chain Stay
-    plotRotated([0, crank_x], [(1/3), crank_y], angle, x);
+    plotRotated([0, crank_x], [(1/3), crank_y], angle, x, y);
     
     % Seat Stay
-    plotRotated([0, seat_stay_x], [(1/3), seat_stay_y], angle, x); 
+    plotRotated([0, seat_stay_x], [(1/3), seat_stay_y], angle, x, y); 
     
     % Seat Tube
-    plotRotated([seat_tube_x, crank_x], [seat_tube_y, crank_y], angle, x);
+    plotRotated([seat_tube_x, crank_x], [seat_tube_y, crank_y], angle, x, y);
     
     % Seat
-    plotRotated([seat_start_x, seat_end_x], [seat_start_y, seat_end_y], angle, x);
+    plotRotated([seat_start_x, seat_end_x], [seat_start_y, seat_end_y], angle, x, y);
     
     % Down Tube
-    plotRotated([crank_x, down_tube_x], [crank_y, down_tube_y], angle, x);
+    plotRotated([crank_x, down_tube_x], [crank_y, down_tube_y], angle, x, y);
     
     % Fork
-    plotRotated([front_hub_x, fork_x], [front_hub_y, fork_y], angle, x);
+    plotRotated([front_hub_x, fork_x], [front_hub_y, fork_y], angle, x, y);
     
     % Handle
-    plotRotated([fork_x, handle_x], [fork_y, handle_y], angle, x);
+    plotRotated([fork_x, handle_x], [fork_y, handle_y], angle, x, y);
     
     % Top Tube
-    plotRotated([seat_stay_x, top_tube_x], [seat_stay_y, top_tube_y], angle, x);
+    plotRotated([seat_stay_x, top_tube_x], [seat_stay_y, top_tube_y], angle, x, y);
     
     % Calf
-    plotRotated([foot_x, knee_x], [foot_y, knee_y], angle, x);
+    plotRotated([foot_x, knee_x], [foot_y, knee_y], angle, x, y);
     
     % Thigh
-    plotRotated([knee_x, hip_x], [knee_y, hip_y], angle, x);
+    plotRotated([knee_x, hip_x], [knee_y, hip_y], angle, x, y);
     
     % Back
-    plotRotated([hip_x, neck_x], [hip_y, neck_y], angle, x);
+    plotRotated([hip_x, neck_x], [hip_y, neck_y], angle, x, y);
     
     % Arm
-    plotRotated([shoulder_x, handle_x], [shoulder_y, handle_y], angle, x);
+    plotRotated([shoulder_x, handle_x], [shoulder_y, handle_y], angle, x, y);
     
     % Head
     [head_xs, head_ys] = getCircle(neck_x, neck_y, (1/5));
-    plotRotated(head_xs, head_ys, angle, x);
+    plotRotated(head_xs, head_ys, angle, x, y);
     
     axis equal;
     hold off;
     
-    function plotRotated(xs, ys, angle, x)
+    function plotRotated(xs, ys, angle, x, y)
         [new_xs, new_ys] = rotate(xs, ys, angle);
-        plot(new_xs*unitLength + x, new_ys*unitHeight, 'k');
+        plot((new_xs - pivot_x)*unitLength + x, (new_ys - pivot_y)*unitHeight + y, 'k');
     end
 end
 
